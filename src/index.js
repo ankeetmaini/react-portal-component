5d00ace1e1219d47d3379cddfdaf7097ba3cab5d
Ankeet Maini <ankeet.maini@gmail.com>
2017-03-15 12:43:37 +0530

    Adds gh-pages deployment changes (#1)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 export default class Portal extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+  }
+
   render () {
     return <h1>I am a portal</h1>;
   }
