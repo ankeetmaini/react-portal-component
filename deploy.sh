748672540ac64a766081ae5930df4e7de760b14e
Ankeet Maini <ankeet.maini@gmail.com>
2017-03-15 14:41:20 +0530

    Fixes file path

--- deploy.sh
+++ deploy.sh
@@ -37,15 +37,9 @@ cd lib
 git config user.name "Travis CI"
 git config user.email "ankeet.maini@gmail.com"
 
-# If there are no changes (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the spec on this push; exiting."
-    exit 0
-fi
-
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
+git add -A
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
@@ -53,7 +47,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in portal_deploy_key.enc -out portal_deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../portal_deploy_key.enc -out ./portal_deploy_key -d
 chmod 600 portal_deploy_key
 eval `ssh-agent -s`
 ssh-add portal_deploy_key
