9542fcf61b0c78200e615c09d99a8ab5973f8d62
Ankeet Maini <ankeet.maini@gmail.com>
2017-03-15 15:14:39 +0530

    Deploy only if there is a change

--- deploy.sh
+++ deploy.sh
@@ -37,6 +37,12 @@ cd lib
 git config user.name "Travis CI"
 git config user.email "ankeet.maini@gmail.com"
 
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A
